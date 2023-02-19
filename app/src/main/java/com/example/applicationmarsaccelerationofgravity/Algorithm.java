3ea281767ac472b83bd2686d3bd13d0f84d01afe
Dmitry <118049037+mitruee@users.noreply.github.com>
2023-02-19 13:27:50 +0300

    Update Algorithm.java

--- app/src/main/java/com/example/applicationmarsaccelerationofgravity/Algorithm.java
+++ app/src/main/java/com/example/applicationmarsaccelerationofgravity/Algorithm.java
@@ -2,36 +2,30 @@ package com.example.applicationmarsaccelerationofgravity;
 
 public class Algorithm {
 
-    // создание поля для алгоритма (скорости света)
-    private final float MASS_FOR_ONE_KG = 40; // метров в секунду
-
-    // создание пустого конструктора
+    
+    private final float MASS_FOR_ONE_KG = 37.8;
+    
     public Algorithm() {
     }
 
-    // метод вычисляющий абсолютную энергию по величине массы
+    
     public long absoluteMassCalculation(String input) {
 
-        /** Необходимо проверить корректность ввода массы тела, так как пользователь может ввести нецелое значение
-         * Java понимает нецелые значения лишь с точкой, если пользователь введёт нецелое число с запятой, то возникнет ошибка
-         * Данную ситуацию нужно отработать
-         */
-        // проверка корректности вводимого значения (с помощью метода contains(",") мы можем проверить есть ли в строке символ ",")
-        if (input.contains(",")) { // если в вводимой строке есть запятая, то
+        
+        if (input.contains(",")) {
 
-            String inputMod = input.replace(",", "."); // то с помощью метода replace() мы поменяем её на точку
+            String inputMod = input.replace(",", ".");
 
-            // конвертирование входного значения из String в float с помощью метода Float.parseFloat()
             float inputFloat = Float.parseFloat(inputMod);
 
-            // возврат вычисленного значения
+            
             return (long) (inputFloat * MASS_FOR_ONE_KG);
 
         } else {
-            // конвертирование входного значения из String в float с помощью метода Long.parseLong()
+            
             float inputFloat = Float.parseFloat(input);
 
-            // возврат вычисленного значения
+            
             return (long) (inputFloat * MASS_FOR_ONE_KG);
         }
     }
